970b3f45d314f2242c5173518b491307dffe6f16
Aoyunpeng <aoyp777@163.com>
2025-12-25 18:10:41 +0800

    修改pom依赖，新建数据库

---
+++ business-adapter/src/main/java/com/demo/adapter/BusinessApplication.java
@@ -0,0 +1,20 @@
+package com.demo.adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * ClassName: BusinessApplication
+ * Package: com.demo.adapter
+ * Description:
+ *
+ * @author ayp
+ * @version 1.0
+ * @date 2025/12/25 16:50
+ */
+
+@SpringBootApplication
+public class BusinessApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessApplication.class, args);
+    }
+}
